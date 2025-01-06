45ddea0be9f308765f63840ce9f676c6ff6a394e
Nikhil909 <m.nikhilraj99@gmail.com>
2025-01-06 18:25:09 +0530

    colours

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/shell-script-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+TIMESTAMP=$(date +%Y-%m-%d %H:%M:%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
